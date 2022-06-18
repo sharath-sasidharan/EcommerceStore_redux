0eab764539b7c1cfdc13185fa08c6beb5c3d35df
Sarath <sasidharansteven@gmail.comm>
2022-06-19 01:20:17 +0530

    Redirection home changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 export const Header = () => {
   return (
     <div className='ui fixed menu'>
         <div className='ui container center'>
-            <h2>Ecommerce-Store</h2>
+            <h1><Link title='Ecommerce store' to={'/'}>E-commerce Store </Link></h1>
         </div>
     </div>
   )
